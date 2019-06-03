c4511717e5a24e4a16d10bed6ac48501d86adca9
ortal azulay levi <44336251+ortallev@users.noreply.github.com>
2019-06-03 21:16:08 +0300

    Add files via upload

---
+++ js/price.js
@@ -0,0 +1,9 @@
+	 $(document).ready(function () { 
+      
+     $(".contentDiv").each( function(index, element) 
+    {
+     $(element).css("background-color", "#f3f1c8");
+    });
+     $(".contentDiv").first().css("background-color", "#F0E68C");
+      $(".contentDiv").last().css("background-color", "#F0E68C");
+      });
